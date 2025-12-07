401c94d8f4a60bd42dea0882a8a24e9326bcade9
Heilotia <47873942+HeilotiaStudio@users.noreply.github.com>
2025-12-07 12:48:17 +0200

    Create session.js

---
+++ session.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sessionData = JSON.parse(localStorage.getItem("supabaseSession"));
+
+  if (!sessionData) {
+    window.location.href = "login.html";
+  } else {
+    document.getElementById("wallet").textContent = sessionData.walletHash;
+    document.getElementById("user-email").textContent = sessionData.user.email;
+  }
+});
